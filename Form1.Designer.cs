80b002587f3618478820870bc3a1756b67a47be6
John Cokins <il7148pg@go.minnstate.edu>
2020-09-08 20:36:11 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace Week3MysteryApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtScore1 = new System.Windows.Forms.TextBox();
+            this.txtScore2 = new System.Windows.Forms.TextBox();
+            this.txtScore3 = new System.Windows.Forms.TextBox();
+            this.txtScore4 = new System.Windows.Forms.TextBox();
+            this.txtScore5 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnCurve = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtLetter1 = new System.Windows.Forms.TextBox();
+            this.txtLetter2 = new System.Windows.Forms.TextBox();
+            this.txtLetter3 = new System.Windows.Forms.TextBox();
+            this.txtLetter4 = new System.Windows.Forms.TextBox();
+            this.txtLetter5 = new System.Windows.Forms.TextBox();
+            this.txtHigh = new System.Windows.Forms.TextBox();
+            this.txtLow = new System.Windows.Forms.TextBox();
+            this.txtAvg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Student 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Student 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Student 5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(93, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Score";
+            // 
+            // txtScore1
+            // 
+            this.txtScore1.Location = new System.Drawing.Point(71, 29);
+            this.txtScore1.Name = "txtScore1";
+            this.txtScore1.Size = new System.Drawing.Size(70, 20);
+            this.txtScore1.TabIndex = 1;
+            // 
+            // txtScore2
+            // 
+            this.txtScore2.Location = new System.Drawing.Point(71, 75);
+            this.txtScore2.Name = "txtScore2";
+            this.txtScore2.Size = new System.Drawing.Size(70, 20);
+            this.txtScore2.TabIndex = 2;
+            // 
+            // txtScore3
+            // 
+            this.txtScore3.Location = new System.Drawing.Point(71, 117);
+            this.txtScore3.Name = "txtScore3";
+            this.txtScore3.Size = new System.Drawing.Size(70, 20);
+            this.txtScore3.TabIndex = 3;
+            // 
+            // txtScore4
+            // 
+            this.txtScore4.Location = new System.Drawing.Point(71, 159);
+            this.txtScore4.Name = "txtScore4";
+            this.txtScore4.Size = new System.Drawing.Size(70, 20);
+            this.txtScore4.TabIndex = 4;
+            // 
+            // txtScore5
+            // 
+            this.txtScore5.Location = new System.Drawing.Point(71, 201);
+            this.txtScore5.Name = "txtScore5";
+            this.txtScore5.Size = new System.Drawing.Size(70, 20);
+            this.txtScore5.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(38, 233);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnCurve
+            // 
+            this.btnCurve.Location = new System.Drawing.Point(150, 233);
+            this.btnCurve.Name = "btnCurve";
+            this.btnCurve.Size = new System.Drawing.Size(75, 23);
+            this.btnCurve.TabIndex = 7;
+            this.btnCurve.Text = "&Adjust Curve";
+            this.btnCurve.UseVisualStyleBackColor = true;
+            this.btnCurve.Click += new System.EventHandler(this.btnCurve_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(296, 204);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Cl&ear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(296, 233);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 9;
+            this.Exit.Text = "E&xit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(226, 72);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "High Score";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(226, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Low Score";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(226, 157);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Average";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(147, 13);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(66, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Letter Grade";
+            // 
+            // txtLetter1
+            // 
+            this.txtLetter1.Location = new System.Drawing.Point(150, 30);
+            this.txtLetter1.Name = "txtLetter1";
+            this.txtLetter1.ReadOnly = true;
+            this.txtLetter1.Size = new System.Drawing.Size(33, 20);
+            this.txtLetter1.TabIndex = 14;
+            this.txtLetter1.TabStop = false;
+            // 
+            // txtLetter2
+            // 
+            this.txtLetter2.Location = new System.Drawing.Point(150, 76);
+            this.txtLetter2.Name = "txtLetter2";
+            this.txtLetter2.ReadOnly = true;
+            this.txtLetter2.Size = new System.Drawing.Size(33, 20);
+            this.txtLetter2.TabIndex = 15;
+            this.txtLetter2.TabStop = false;
+            // 
+            // txtLetter3
+            // 
+            this.txtLetter3.Location = new System.Drawing.Point(150, 118);
+            this.txtLetter3.Name = "txtLetter3";
+            this.txtLetter3.ReadOnly = true;
+            this.txtLetter3.Size = new System.Drawing.Size(33, 20);
+            this.txtLetter3.TabIndex = 16;
+            this.txtLetter3.TabStop = false;
+            // 
+            // txtLetter4
+            // 
+            this.txtLetter4.Location = new System.Drawing.Point(150, 160);
+            this.txtLetter4.Name = "txtLetter4";
+            this.txtLetter4.ReadOnly = true;
+            this.txtLetter4.Size = new System.Drawing.Size(33, 20);
+            this.txtLetter4.TabIndex = 17;
+            this.txtLetter4.TabStop = false;
+            // 
+            // txtLetter5
+            // 
+            this.txtLetter5.Location = new System.Drawing.Point(150, 202);
+            this.txtLetter5.Name = "txtLetter5";
+            this.txtLetter5.ReadOnly = true;
+            this.txtLetter5.Size = new System.Drawing.Size(33, 20);
+            this.txtLetter5.TabIndex = 18;
+            this.txtLetter5.TabStop = false;
+            // 
+            // txtHigh
+            // 
+            this.txtHigh.Location = new System.Drawing.Point(296, 69);
+            this.txtHigh.Name = "txtHigh";
+            this.txtHigh.ReadOnly = true;
+            this.txtHigh.Size = new System.Drawing.Size(63, 20);
+            this.txtHigh.TabIndex = 19;
+            this.txtHigh.TabStop = false;
+            // 
+            // txtLow
+            // 
+            this.txtLow.Location = new System.Drawing.Point(296, 112);
+            this.txtLow.Name = "txtLow";
+            this.txtLow.ReadOnly = true;
+            this.txtLow.Size = new System.Drawing.Size(63, 20);
+            this.txtLow.TabIndex = 20;
+            this.txtLow.TabStop = false;
+            // 
+            // txtAvg
+            // 
+            this.txtAvg.Location = new System.Drawing.Point(296, 154);
+            this.txtAvg.Name = "txtAvg";
+            this.txtAvg.ReadOnly = true;
+            this.txtAvg.Size = new System.Drawing.Size(63, 20);
+            this.txtAvg.TabIndex = 21;
+            this.txtAvg.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 281);
+            this.Controls.Add(this.txtAvg);
+            this.Controls.Add(this.txtLow);
+            this.Controls.Add(this.txtHigh);
+            this.Controls.Add(this.txtLetter5);
+            this.Controls.Add(this.txtLetter4);
+            this.Controls.Add(this.txtLetter3);
+            this.Controls.Add(this.txtLetter2);
+            this.Controls.Add(this.txtLetter1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCurve);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtScore5);
+            this.Controls.Add(this.txtScore4);
+            this.Controls.Add(this.txtScore3);
+            this.Controls.Add(this.txtScore2);
+            this.Controls.Add(this.txtScore1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtScore1;
+        private System.Windows.Forms.TextBox txtScore2;
+        private System.Windows.Forms.TextBox txtScore3;
+        private System.Windows.Forms.TextBox txtScore4;
+        private System.Windows.Forms.TextBox txtScore5;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnCurve;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtLetter1;
+        private System.Windows.Forms.TextBox txtLetter2;
+        private System.Windows.Forms.TextBox txtLetter3;
+        private System.Windows.Forms.TextBox txtLetter4;
+        private System.Windows.Forms.TextBox txtLetter5;
+        private System.Windows.Forms.TextBox txtHigh;
+        private System.Windows.Forms.TextBox txtLow;
+        private System.Windows.Forms.TextBox txtAvg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week3MysteryApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            List<double> listOfScores = convertInputToList();
+
+            HighLowAvg(listOfScores);
+
+            List<string> listOfGrades = new List<string>();
+
+            ConvertToLetterGrades(listOfScores, listOfGrades);
+
+        }
+
+        private void ConvertToLetterGrades(List<double> listOfScores, List<string> listOfGrades)
+        {
+            foreach (double score in listOfScores)
+            {
+                string abcdf = "";
+
+                if (score >= 90)
+                {
+                    abcdf = "A";
+                }
+                else if (score >= 80)
+                {
+                    abcdf = "B";
+                }
+                else if (score >= 70)
+                {
+                    abcdf = "C";
+                }
+                else if (score >= 60)
+                {
+                    abcdf = "D";
+                }
+                else
+                {
+                    abcdf = "F-";
+                }
+                listOfGrades.Add(abcdf);
+            }
+
+            txtLetter1.Text = listOfGrades[0];
+            txtLetter2.Text = listOfGrades[1];
+            txtLetter3.Text = listOfGrades[2];
+            txtLetter4.Text = listOfGrades[3];
+            txtLetter5.Text = listOfGrades[4];
+        }
+
+        private void HighLowAvg(List<double> listOfScores)
+        {
+            double HighScore = listOfScores.Max();
+            txtHigh.Text = HighScore.ToString();
+
+            double LowScore = listOfScores.Min();
+            txtLow.Text = LowScore.ToString();
+
+            double Average = listOfScores.Average();
+            txtAvg.Text = Average.ToString();
+        }
+
+        private List<double> convertInputToList()
+        {
+            double Student1Score = Convert.ToDouble(txtScore1.Text);
+            double Student2Score = Convert.ToDouble(txtScore2.Text);
+            double Student3Score = Convert.ToDouble(txtScore3.Text);
+            double Student4Score = Convert.ToDouble(txtScore4.Text);
+            double Student5Score = Convert.ToDouble(txtScore5.Text);
+
+            List<double> listOfScores = new List<double>()
+                {
+                Student1Score,
+                Student2Score,
+                Student3Score,
+                Student4Score,
+                Student5Score
+                };
+            return listOfScores;
+        }
+
+        private void btnCurve_Click(object sender, EventArgs e)
+        {
+            List<double> listOfScores = convertInputToList();
+
+            HighLowAvg(listOfScores);
+
+            List<string> listOfGrades = new List<string>();
+
+            int newA = Convert.ToInt32(listOfScores.Max());
+            int newB = newA - 20;
+            int newC = newB - 10;
+            int newD = newC - 10;
+
+            foreach (double score in listOfScores)
+            {
+                string abcdf = "";
+
+                if (score >= newA)
+                {
+                    abcdf = "A";
+                }
+                else if (score >= newB)
+                {
+                    abcdf = "B";
+                }
+                else if (score >= newC)
+                {
+                    abcdf = "C";
+                }
+                else if (score >= newD)
+                {
+                    abcdf = "D";
+                }
+                else
+                {
+                    abcdf = "F-";
+                }
+                listOfGrades.Add(abcdf);
+            }
+
+            txtLetter1.Text = listOfGrades[0];
+            txtLetter2.Text = listOfGrades[1];
+            txtLetter3.Text = listOfGrades[2];
+            txtLetter4.Text = listOfGrades[3];
+            txtLetter5.Text = listOfGrades[4];
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtLetter1.Text = string.Empty;
+            txtLetter2.Text = string.Empty;
+            txtLetter3.Text = string.Empty;
+            txtLetter4.Text = string.Empty;
+            txtLetter5.Text = string.Empty;
+
+            txtScore1.Text = string.Empty;
+            txtScore2.Text = string.Empty;
+            txtScore3.Text = string.Empty;
+            txtScore4.Text = string.Empty;
+            txtScore5.Text = string.Empty;
+
+            txtHigh.Text = string.Empty;
+            txtLow.Text = string.Empty;
+            txtAvg.Text = string.Empty;
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
